25aca255c3d0b2ac83d66b467fbb81613d132fc0
shunsuke.kobayashi <id25gk@yahoo.co.jp>
2023-06-08 13:57:18 +0900

    ページネーションを追加

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -26,12 +26,46 @@ public class IndexServlet extends HttpServlet {
 
         EntityManager em = DBUtil.createEntityManager();
 
-        List<Message> messages = em.createNamedQuery
-                ("getAllMessages", Message.class).getResultList();
+//      開くページ数を取得（デフォルトは1ページ目）
+        int page = 1;
+        try {
+            page = Integer.parseInt(request.getParameter("page"));
+        } catch(NumberFormatException e) {}
+
+ //     最大件数と開始位置を指定してメッセージを取得
+        List<Message> messages = em.createNamedQuery("getAllMessages", Message.class)
+                                   .setFirstResult(15 * (page - 1))
+                                   .setMaxResults(15)
+                                   .getResultList();
+
+//      全件数を取得
+        long messages_count = (long)em.createNamedQuery("getMessagesCount", Long.class)
+                                      .getSingleResult();
 
         em.close();
 
         request.setAttribute("messages", messages);
+        request.setAttribute("messages_count", messages_count);     //全件数
+        request.setAttribute("page", page);                         //ページ数
+
+//      page=2 とあっても request.getParameter() で取得できるのはString型の "2" です。
+//      そこで Integer.parseInt() で文字列から数値に変更しています。
+//
+//      ただ、page のパラメータが無かったり page=a のように数値ではないものを、利用者が指定する可能性はあります。
+//      本来ならしっかりした形でパラメータが数値かどうか調べるべきですが、今回は、数値ではないものを
+//      Integer.parseInt に渡すと NumberFormatException という例外が表示されるという性質を使って、
+//      try と catch で囲って処理が止まらないようにしています。
+//
+//      データベースへ問い合わせている部分ですが、setFirstResult(15 * (page - 1)) は
+//      「何件目からデータを取得するか（配列と同じ0番目から数えていきます）」、
+//      setMaxResults(15) は「データの最大取得件数（今回は15件で固定）」を設定します。
+//
+//      また、getAllMessages は複数のデータが結果として戻ってくる可能性があるため getResultList() で
+//      問い合わせ結果を取得していますが、getMessagesCount は全件数という1つの結果のみが戻ってくるので、
+//      getSingleResult() という “1件だけ取得する” という命令を指定しています。
+//
+//      最後にメッセージデータのリストの他、全件数の数値と表示するページ数の数値もリクエストスコープにセットして
+//      index.jsp に送っています。
 
 //      フラッシュメッセージがセッションスコープにセットされていたら
 //      リクエストスコープに保存する（セッションスコープからは削除）
